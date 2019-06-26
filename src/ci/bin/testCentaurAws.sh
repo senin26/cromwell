57f02ee098538f8ed1a3a5ea14318bd9355abec0
Sergey Ionin <36512667+senin26@users.noreply.github.com>
2019-06-26 19:37:41 +0300

    Update testCentaurAws.sh
    with only  -i hello \ and
        -i long_cmd \

--- src/ci/bin/testCentaurAws.sh
+++ src/ci/bin/testCentaurAws.sh
@@ -19,31 +19,9 @@ cromwell::build::pip_install awscli --upgrade
 
 
 cromwell::build::run_centaur \
-    -p 100 \
-    -e smartseq2singlesample \
-    -e arrays \
-    -e haplotypecaller \
-    -e jointdiscovery \
-    -e mutect2 \
-    -e singlesample \
-    -e singlesample_production \
-    -e cnv_somatic_pair \
-    -e cnv_somatic_panel \
-    -e localdockertest \
-    -e inline_file \
-    -e iwdr_input_string_function \
-    -e non_root_default_user \
-    -e cwl_interpolated_strings \
-    -e non_root_specified_user \
-    -e space \
-    -e draft3_optional_input_from_scatter \
-    -e iwdr_input_string \
-    -e cwl_cache_between_workflows \
-    -e abort.scheduled_abort \
-    -e cwl_cache_within_workflow \
-    -e inline_file_custom_entryname \
-    -e relative_output_paths \
-    -e relative_output_paths_colliding \
-    -e standard_output_paths_colliding_prevented \
+    -i hello \
+    -i long_cmd \
+    #-i haplotypecaller.aws \
+    # -i singlesample.aws \
 
 cromwell::build::generate_code_coverage
